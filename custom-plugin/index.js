1a46bb1c19bfb02990db88aa460621e78559165c
orlyd <orlyd@wix.com>
2024-08-22 09:44:15 +0300

    add advanced implementation of the rule

--- custom-plugin/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const noConsoleLog_1 = require("./noConsoleLog");
-const plugin = {
-    rules: {
-        'noConsoleLog': noConsoleLog_1.noConsoleLog
-    }
-};
-module.exports = plugin;
